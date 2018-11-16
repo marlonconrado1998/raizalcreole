f7d2877ca6e385dfcfaf093370af11c9c3f1a9ab
elconrado <marlon.conrado@zabud.co>
2018-11-16 13:37:21 -0500

    Arreglando problemas

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,18 +1,18 @@
 var jwt = require('jsonwebtoken');
 
 let verifyToken = (req, res, next) => {
-    // let token = req.headers.authorization;
-    // if (token) {
-    //     jwt.verify(token, process.env.JWT_SECRET, (error, resp) => {
-    //         if (resp) {
-    //             next();
-    //         } else if (error) {
-    //             res.status(401).send("Unauthorized");
-    //         }
-    //     });
-    // } else {
-    //     res.status(401).send("Unauthorized");
-    // }
+    let token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (error, resp) => {
+            if (resp) {
+                next();
+            } else if (error) {
+                res.status(401).send("Unauthorized");
+            }
+        });
+    } else {
+        res.status(401).send("Unauthorized");
+    }
 }
 
 
